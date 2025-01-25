65205e13e6adf282e134c67ba51bc015d1860f19
Hossam Rakha <hossamrakha0@gmail.com>
2025-01-25 17:46:20 +0200

    completed the todo sample project

--- app/src/main/java/com/hos/compose/coroutinesroomcompose/MainActivity.kt
+++ app/src/main/java/com/hos/compose/coroutinesroomcompose/MainActivity.kt
@@ -9,55 +9,45 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.room.Room
 import com.hos.compose.coroutinesroomcompose.composables.TodoInputBar
 import com.hos.compose.coroutinesroomcompose.composables.TodoItemsContainer
 import com.hos.compose.coroutinesroomcompose.data.models.TodoItem
+import com.hos.compose.coroutinesroomcompose.data.room.AppDatabase
+import com.hos.compose.coroutinesroomcompose.repository.TodoRepository
 import com.hos.compose.coroutinesroomcompose.ui.constants.OverlappingHeight
 import com.hos.compose.coroutinesroomcompose.ui.theme.CoroutinesroomsampleTheme
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.flowOf
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // 1. Manual DB Creation
+        val db = Room
+            .databaseBuilder(applicationContext, AppDatabase::class.java, "todo-db")
+            .build()
+        // 2. Manual MainViewModel Creation
+        val mainViewModel = MainViewModel(TodoRepository(db.todoDao()), ioDispatcher = Dispatchers.IO)
         setContent {
             Box(
                 modifier = Modifier.fillMaxSize()
             ) {
                 TodoItemsContainer(
-                    // 1. Mock Data for Todo Items
-                    todoItemsFlow = flowOf(
-                        listOf(
-                            TodoItem(title = "Todo Item 1"),
-                            TodoItem(title = "Todo Item 2", isDone = true),
-                            TodoItem(title = "Todo Item 3"),
-                            TodoItem(title = "Todo Item 4", isDone = true),
-                            TodoItem(title = "Todo Item 5"),
-                            TodoItem(title = "Todo Item 6"),
-                            TodoItem(title = "Todo Item 7"),
-                            TodoItem(title = "Todo Item 8"),
-                            TodoItem(title = "Todo Item 9"),
-                            TodoItem(title = "Todo Item 10"),
-                            TodoItem(title = "Todo Item 11", isDone = true),
-                            TodoItem(title = "Todo Item 12"),
-                            TodoItem(title = "Todo Item 13"),
-                            TodoItem(title = "Todo Item 14"),
-                            TodoItem(title = "Todo Item 15")
-                        )
-                    ),
-                    onItemClick = {},
-                    onItemDelete = {},
-                    // 2. Space Adjustment for Overlapping UI Elements
+                    todoItemsFlow = mainViewModel.todos,
+                    // 3. Method Reference Expression
+                    onItemClick = mainViewModel::toggleTodo,
+                    onItemDelete = mainViewModel::removeTodo,
                     overlappingElementsHeight = OverlappingHeight
                 )
                 TodoInputBar(
                     modifier = Modifier.align(Alignment.BottomStart),
-                    onAddButtonClick = {}
+                    onAddButtonClick = mainViewModel::addTodo
                 )
             }
         }
     }
 }
-
 
 
 @Preview(showBackground = true)
--- app/src/main/java/com/hos/compose/coroutinesroomcompose/MainViewModel.kt
+++ app/src/main/java/com/hos/compose/coroutinesroomcompose/MainViewModel.kt
@@ -1,6 +1,32 @@
 package com.hos.compose.coroutinesroomcompose
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hos.compose.coroutinesroomcompose.data.models.TodoItem
+import com.hos.compose.coroutinesroomcompose.repository.TodoRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.launch
 
-class MainViewModel : ViewModel() {
+class MainViewModel(
+    // 1. ViewModel Initialization
+    private val repository: TodoRepository,
+    private val ioDispatcher: CoroutineDispatcher
+) : ViewModel() {
+
+    /// TODO: Lesson -> The repository provides access to the data layer, allowing the ViewModel to perform database operations through it
+    /// TODO: Lesson -> The CoroutineDispatcher, typically Dispatchers.IO for database operations, is used to ensure these operations are executed on a background thread, keeping the UI thread free from blocking operations. This setup effectively separates the concerns of data management and UI logic
+    /// TODO: Lesson -> Flow establishes a reactive data stream, where updates in the database are automatically reflected in the UI. The use of Kotlin's Flow ensures asynchronous data handling and lifecycle-aware updates, making the UI responsive and up-to-date with the latest data without manual refreshes
+
+    // 2. Todo Data Flow
+    val todos = repository.allTodos
+
+    // 3. Todo Operations
+    fun addTodo(todo: String) =
+        viewModelScope.launch(ioDispatcher) { repository.insert(TodoItem(title = todo)) }
+
+    fun toggleTodo(todoItem: TodoItem) =
+        viewModelScope.launch(ioDispatcher) { repository.insert(todoItem.copy(isDone = !todoItem.isDone)) }
+
+    fun removeTodo(todoItem: TodoItem) =
+        viewModelScope.launch(ioDispatcher) { repository.delete(todoItem) }
 }
