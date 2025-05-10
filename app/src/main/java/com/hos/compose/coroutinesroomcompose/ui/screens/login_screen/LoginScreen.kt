34ca2c1c5ad9f782640f9a1dea6ffc40fa03eb5d
Hossam Rakha <hossamrakha0@gmail.com>
2025-05-10 17:21:52 +0300

    converted emailField to outlineEmailField

--- app/src/main/java/com/hos/compose/coroutinesroomcompose/ui/screens/login_screen/LoginScreen.kt
+++ app/src/main/java/com/hos/compose/coroutinesroomcompose/ui/screens/login_screen/LoginScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -15,6 +16,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hos.compose.coroutines_room_sample.ui.theme.*
@@ -42,7 +45,7 @@ fun LoginScreen(
             Spacer(modifier = Modifier.height(24.dp))
 
 
-            TextField(
+            OutlinedTextField(
                 modifier = Modifier
                     .height(60.dp)
                     .fillMaxWidth(),
@@ -51,6 +54,10 @@ fun LoginScreen(
                 label = {
                     Text(text = stringResource(id = R.string.email_hint))
                 },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Next
+                ),
                 singleLine = true,
             )
 
